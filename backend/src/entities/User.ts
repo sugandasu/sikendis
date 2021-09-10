dbc07521ff19276b860aa3aaa7f6350d389d866e
Arif Suganda <arifsuganda.su@gmail.com>
2021-09-10 21:31:10 +0800

    create graphql schema

--- backend/src/entities/User.ts
+++ backend/src/entities/User.ts
@@ -1,28 +1,36 @@
+import { Field, ObjectType } from "type-graphql";
 import {
   Entity,
   PrimaryGeneratedColumn,
   Column,
   CreateDateColumn,
   UpdateDateColumn,
+  BaseEntity,
 } from "typeorm";
 
+@ObjectType()
 @Entity()
-export class User {
+export class User extends BaseEntity {
+  @Field()
   @PrimaryGeneratedColumn()
   id!: number;
 
+  @Field()
   @Column({ unique: true })
   username!: string;
 
+  @Field()
   @Column({ unique: true })
   email!: string;
 
   @Column()
   password!: string;
 
+  @Field()
   @CreateDateColumn()
   createdAt: Date;
 
+  @Field()
   @UpdateDateColumn()
   updatedAt: Date;
 }
