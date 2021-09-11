8c741ae7ad023a386af19ce41db6f3ba3fcec793
Arif Suganda <arifsuganda.su@gmail.com>
2021-09-11 20:16:40 +0800

    add tambah button

--- frontend/src/pages/dashboard/pengguna/index.tsx
+++ frontend/src/pages/dashboard/pengguna/index.tsx
@@ -1,13 +1,20 @@
-import { Box } from "@chakra-ui/layout";
+import { Box, Link } from "@chakra-ui/layout";
 import { Table, TableCaption, Tbody, Th, Thead, Tr } from "@chakra-ui/table";
 import React from "react";
 import { useIsAuth } from "../../../middlewares/useIsAuth";
+import NextLink from "next/link";
+import { Button } from "@chakra-ui/button";
 
 const DashboardPenggunaIndex: React.FC<{}> = ({}) => {
   useIsAuth();
   return (
     <Box>
       <Box>Pengguna Index</Box>
+      <NextLink href="/dashboard/pengguna/tambah">
+        <Link>
+          <Button>Tambah</Button>
+        </Link>
+      </NextLink>
       <Table>
         <TableCaption>Pengguna kendaraan</TableCaption>
         <Thead>
