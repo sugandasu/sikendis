3b6da5dd34a7d01377f9aadf51d68f22c97179ba
Arif Suganda <arifsuganda.su@gmail.com>
2021-09-18 17:50:18 +0800

    delete unused import

--- frontend/src/pages/dashboard/index.tsx
+++ frontend/src/pages/dashboard/index.tsx
@@ -1,5 +1,5 @@
 import { Box, Text } from "@chakra-ui/layout";
-import { Heading, Stack } from "@chakra-ui/react";
+import { Stack } from "@chakra-ui/react";
 import React from "react";
 import { DashboardLayout } from "../../components/DashboardLayout";
 
