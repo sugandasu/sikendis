b4edf05d0fe89f821e794041ce17b39500d86cb6
Arif Suganda <arifsuganda.su@gmail.com>
2021-09-19 09:46:57 +0800

    fix pengguna edit

--- frontend/src/pages/dashboard/pengguna/edit/[id].tsx
+++ frontend/src/pages/dashboard/pengguna/edit/[id].tsx
@@ -1,8 +1,11 @@
 import { Button } from "@chakra-ui/button";
 import { Box } from "@chakra-ui/layout";
+import { Flex, Link, Stack, Text } from "@chakra-ui/react";
 import { Form, Formik } from "formik";
 import { useRouter } from "next/dist/client/router";
+import NextLink from "next/link";
 import React from "react";
+import { DashboardLayout } from "../../../../components/DashboardLayout";
 import { InputField } from "../../../../components/InputField";
 import {
   usePenggunaQuery,
@@ -14,6 +17,12 @@ import { toErrorMap } from "../../../../utils/toErrorMap";
 
 const DashboardPenggunaEdit: React.FC<{}> = ({}) => {
   useIsOperator();
+  const breadCrumbs = [
+    { text: "Dashboard", link: "/dashboard", isCurrentPage: false },
+    { text: "Pengguna", link: "/dashboard/pengguna", isCurrentPage: false },
+    { text: "Edit", link: "#", isCurrentPage: true },
+  ];
+
   const router = useRouter();
   const intId = useGetIntId();
   const { data, loading } = usePenggunaQuery({
@@ -29,60 +38,81 @@ const DashboardPenggunaEdit: React.FC<{}> = ({}) => {
     return <Box>Data tidak ditemukan</Box>;
   }
   return (
-    <Box>
-      <Box>Tambah Pengguna</Box>
-      <Formik
-        initialValues={{
-          nip: data.pengguna.nip,
-          nama: data.pengguna.nama,
-          jabatan: data.pengguna.jabatan,
-          instansi: data.pengguna.instansi,
-          subBagian: data.pengguna.subBagian,
-          fotoProfil: data.pengguna.fotoProfil,
-        }}
-        onSubmit={async (values, { setErrors }) => {
-          const response = await updatePengguna({
-            variables: { id: intId, payload: values },
-          });
-          if (response.data?.updatePengguna.errors) {
-            setErrors(toErrorMap(response.data.updatePengguna.errors));
-          } else if (response.data?.updatePengguna.pengguna) {
-            router.push("/dashboard/pengguna");
-          }
-        }}
-      >
-        {({ isSubmitting }) => (
-          <Form>
-            <InputField name="nip" label="Nip" placeholder="nip" />
-            <InputField name="nama" label="Nama" placeholder="nama" />
-            <InputField name="jabatan" label="Jabatan" placeholder="jabatan" />
-            <InputField
-              name="instansi"
-              label="Instansi"
-              placeholder="instansi"
-            />
-            <InputField
-              name="subBagian"
-              label="Sub bagian"
-              placeholder="subBagian"
-            />
-            <InputField
-              name="fotoProfil"
-              label="Foto profil"
-              placeholder="fotoProfil"
-            />
-            <Button
-              mt={4}
-              type="submit"
-              isLoading={isSubmitting}
-              colorScheme="teal"
-            >
-              Edit Pengguna
-            </Button>
-          </Form>
-        )}
-      </Formik>
-    </Box>
+    <DashboardLayout headerText="Dashboard" breadCrumbs={breadCrumbs}>
+      <Stack>
+        <Box borderWidth="1px" borderRadius="md">
+          <Box p={5}>
+            <Flex align="center" justifyContent="space-between" mb={2}>
+              <Text fontSize="l">Edit Pengguna</Text>
+              <NextLink href="/dashboard/pengguna">
+                <Link>
+                  <Button bg="red.500" color="white">
+                    Kembali
+                  </Button>
+                </Link>
+              </NextLink>
+            </Flex>
+            <Box>
+              <Formik
+                initialValues={{
+                  nip: data.pengguna.nip,
+                  nama: data.pengguna.nama,
+                  jabatan: data.pengguna.jabatan,
+                  instansi: data.pengguna.instansi,
+                  subBagian: data.pengguna.subBagian,
+                  fotoProfil: data.pengguna.fotoProfil,
+                }}
+                onSubmit={async (values, { setErrors }) => {
+                  const response = await updatePengguna({
+                    variables: { id: intId, payload: values },
+                  });
+                  if (response.data?.updatePengguna.errors) {
+                    setErrors(toErrorMap(response.data.updatePengguna.errors));
+                  } else if (response.data?.updatePengguna.pengguna) {
+                    router.push("/dashboard/pengguna");
+                  }
+                }}
+              >
+                {({ isSubmitting }) => (
+                  <Form>
+                    <InputField name="nip" label="Nip" placeholder="nip" />
+                    <InputField name="nama" label="Nama" placeholder="nama" />
+                    <InputField
+                      name="jabatan"
+                      label="Jabatan"
+                      placeholder="jabatan"
+                    />
+                    <InputField
+                      name="instansi"
+                      label="Instansi"
+                      placeholder="instansi"
+                    />
+                    <InputField
+                      name="subBagian"
+                      label="Sub bagian"
+                      placeholder="subBagian"
+                    />
+                    <InputField
+                      name="fotoProfil"
+                      label="Foto profil"
+                      placeholder="fotoProfil"
+                    />
+                    <Button
+                      mt={4}
+                      type="submit"
+                      isLoading={isSubmitting}
+                      colorScheme="teal"
+                    >
+                      Edit Pengguna
+                    </Button>
+                  </Form>
+                )}
+              </Formik>
+            </Box>
+          </Box>
+        </Box>
+      </Stack>
+    </DashboardLayout>
   );
 };
 
