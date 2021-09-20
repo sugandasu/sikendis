687eb326bf9dd3ade618683244acb75df8fa44df
Arif Suganda <arifsuganda.su@gmail.com>
2021-09-20 19:12:07 +0800

    create file field

---
+++ frontend/src/components/FileField.tsx
@@ -0,0 +1,39 @@
+import {
+  FormControl,
+  FormErrorMessage,
+  FormLabel,
+  Input,
+} from "@chakra-ui/react";
+import { useField } from "formik";
+import { InputHTMLAttributes } from "react";
+
+type FileFieldProps = InputHTMLAttributes<HTMLInputElement> & {
+  name: string;
+  label: string;
+  setFile: (file) => void;
+};
+
+export const FileField: React.FC<FileFieldProps> = ({
+  name,
+  label,
+  size: _,
+  setFile,
+  ...props
+}) => {
+  // const [field, { error }] = useField(props);
+  return (
+    <FormControl mb={2}>
+      <FormLabel htmlFor={name}>{label}</FormLabel>
+      <Input
+        type="file"
+        {...props}
+        id={name}
+        onChange={(event) => {
+          setFile(event.currentTarget.files[0]);
+          console.log(event.currentTarget.files[0]);
+        }}
+      />
+      {/* {!!error ? <FormErrorMessage>{error}</FormErrorMessage> : null} */}
+    </FormControl>
+  );
+};
