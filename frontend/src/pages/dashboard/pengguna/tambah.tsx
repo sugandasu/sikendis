7d8d232625d07b6afd3189095f071e2ddf5cf20d
Arif Suganda <arifsuganda.su@gmail.com>
2021-09-20 19:11:49 +0800

    add react dropzone

--- frontend/src/pages/dashboard/pengguna/tambah.tsx
+++ frontend/src/pages/dashboard/pengguna/tambah.tsx
@@ -1,15 +1,24 @@
+import { useMutation } from "@apollo/client";
 import { Button } from "@chakra-ui/button";
 import { Box } from "@chakra-ui/layout";
 import { Flex, Link, Stack, Text } from "@chakra-ui/react";
 import { Form, Formik } from "formik";
+import gql from "graphql-tag";
 import { useRouter } from "next/dist/client/router";
 import NextLink from "next/link";
-import React from "react";
+import React, { useCallback, useState } from "react";
+import { useDropzone } from "react-dropzone";
 import { DashboardLayout } from "../../../components/DashboardLayout";
 import { InputField } from "../../../components/InputField";
 import { useCreatePenggunaMutation } from "../../../generated/graphql";
 import { useIsOperator } from "../../../middlewares/useIsOperator";
 import { toErrorMap } from "../../../utils/toErrorMap";
+
+const uploadFileMutation = gql`
+  mutation UploadImage($file: Upload!) {
+    singleUpload(file: $file)
+  }
+`;
 
 const DashboardPenggunaTambah: React.FC<{}> = ({}) => {
   useIsOperator();
@@ -20,6 +29,53 @@ const DashboardPenggunaTambah: React.FC<{}> = ({}) => {
   ];
   const router = useRouter();
   const [createPengguna] = useCreatePenggunaMutation();
+
+  const [upload] = useMutation(uploadFileMutation);
+  const [fileToUpload, setFileToUpload] = useState<File>();
+
+  const readFile = (file: File) => {
+    return new Promise((resolve) => {
+      const reader = new FileReader();
+      reader.addEventListener("load", () => resolve(reader.result), false);
+      reader.readAsDataURL(file);
+    });
+  };
+
+  const onFileChange = async (e: any) => {
+    setShowCrop(true);
+    const file = e;
+    let imageDataUrl: any = await readFile(file);
+
+    setImageSrc(imageDataUrl);
+  };
+
+  const onDrop = useCallback(
+    ([file]) => {
+      console.log([file]);
+      console.log("single file", file);
+      console.log("file 0:", file[0]);
+      setFileToUpload(file);
+      onFileChange(file);
+    },
+    [upload]
+  );
+
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
+
+  const InputDrop = () => {
+    if (fileToUpload) {
+      return <Text>📷 ✅</Text>;
+    } else if (isDragActive) {
+      return <Text>Drop the image here</Text>;
+    } else {
+      return (
+        <Text>
+          Drag 'n' drop your image here, or just click to select an image🍎
+        </Text>
+      );
+    }
+  };
+
   return (
     <DashboardLayout headerText="Dashboard" breadCrumbs={breadCrumbs}>
       <Stack>
@@ -43,11 +99,13 @@ const DashboardPenggunaTambah: React.FC<{}> = ({}) => {
                   jabatan: "",
                   instansi: "",
                   subBagian: "",
-                  fotoProfil: "",
                 }}
                 onSubmit={async (values, { setErrors }) => {
                   const response = await createPengguna({
-                    variables: values,
+                    variables: {
+                      payload: values,
+                      fotoProfil: fileToUpload,
+                    },
                     update: (cache) => {
                       cache.evict({ fieldName: "penggunas" });
                     },
@@ -78,11 +136,19 @@ const DashboardPenggunaTambah: React.FC<{}> = ({}) => {
                       label="Sub bagian"
                       placeholder="subBagian"
                     />
-                    <InputField
-                      name="fotoProfil"
-                      label="Foto profil"
-                      placeholder="fotoProfil"
-                    />
+                    <Box
+                      mt={2}
+                      mb={4}
+                      borderColor="dark"
+                      borderStyle="dashed"
+                      borderWidth="2px"
+                      borderRadius="lg"
+                      p={5}
+                      {...getRootProps()}
+                    >
+                      <input {...getInputProps()} />
+                      <InputDrop></InputDrop>
+                    </Box>
                     <Button
                       mt={4}
                       type="submit"
