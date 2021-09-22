77586054948cea54848ec4347d5613afe0bb4f35
Arif Suganda <arifsuganda.su@gmail.com>
2021-09-22 20:14:14 +0800

    add peminjaman columns

--- backend/src/entities/Peminjaman.ts
+++ backend/src/entities/Peminjaman.ts
@@ -1,4 +1,7 @@
+import { Field } from "type-graphql";
 import {
+  BaseEntity,
+  Column,
   CreateDateColumn,
   Entity,
   JoinColumn,
@@ -10,17 +13,51 @@ import { Kendaraan } from "./Kendaraan";
 import { Pengguna } from "./Pengguna";
 
 @Entity()
-export class Peminjaman {
+export class Peminjaman extends BaseEntity {
   @PrimaryGeneratedColumn()
   id!: number;
 
+  @Field()
   @OneToOne(() => Kendaraan)
   @JoinColumn()
   kendaraan: Kendaraan;
 
+  @Field()
   @OneToOne(() => Pengguna)
   @JoinColumn()
   pengguna: Pengguna;
+
+  @Field()
+  @Column()
+  nomorDisposisi!: string;
+
+  @Field()
+  @Column()
+  fileDisposisi!: string;
+
+  @Field()
+  @Column()
+  nomorSuratPermohonan!: string;
+
+  @Field()
+  @Column()
+  fileSuratPermohonan!: string;
+
+  @Field()
+  @Column({ type: "date" })
+  tanggalMulai!: Date;
+
+  @Field()
+  @Column({ type: "date" })
+  tanggalSelesai!: Date;
+
+  @Field()
+  @Column()
+  nomorHpSupir!: string;
+
+  @Field(() => String, { nullable: true })
+  @Column({ type: "varchar", nullable: true })
+  keterangan!: string | null;
 
   @CreateDateColumn()
   createAt: Date;
