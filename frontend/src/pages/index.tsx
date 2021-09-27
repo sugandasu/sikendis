78bdd0635d49da52a5abf9f1a47e2296e93f256a
Arif Suganda <arifsuganda.su@gmail.com>
2021-09-27 11:48:54 +0800

    delete unused code

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -16,39 +16,14 @@ import { Main } from "../components/Main";
 
 const Index = () => (
   <Container height="100vh">
-    <Hero />
     <Main>
-      <Text>
-        Example repository of <Code>Next.js</Code> + <Code>chakra-ui</Code> +{" "}
-        <Code>typescript</Code>.
-      </Text>
-
-      <List spacing={3} my={0}>
-        <ListItem>
-          <ListIcon as={CheckCircleIcon} color="green.500" />
-          <ChakraLink
-            isExternal
-            href="https://chakra-ui.com"
-            flexGrow={1}
-            mr={2}
-          >
-            Chakra UI <LinkIcon />
-          </ChakraLink>
-        </ListItem>
-        <ListItem>
-          <ListIcon as={CheckCircleIcon} color="green.500" />
-          <ChakraLink isExternal href="https://nextjs.org" flexGrow={1} mr={2}>
-            Next.js <LinkIcon />
-          </ChakraLink>
-        </ListItem>
-      </List>
+      <Text>Sistem</Text>
     </Main>
 
     <DarkModeSwitch />
     <Footer>
       <Text>Next ❤️ Chakra</Text>
     </Footer>
-    <CTA />
   </Container>
 );
 
