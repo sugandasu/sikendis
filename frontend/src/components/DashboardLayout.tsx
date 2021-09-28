ec79e3e3f400feb2880eba48eade729c77ec5fff
Arif Suganda <arifsuganda.su@gmail.com>
2021-09-28 23:23:49 +0800

    fix gray box error

--- frontend/src/components/DashboardLayout.tsx
+++ frontend/src/components/DashboardLayout.tsx
@@ -44,22 +44,26 @@ export const DashboardLayout: React.FC<DashboardLayoutProps> = ({
             setMenuIsClose={setMenuIsClose}
           />
         </Box>
-        <Box
-          position="absolute"
-          bgColor="gray.600"
-          width="100%"
-          height="150px"
-          zIndex={-1}
-        ></Box>
-        <Box mt={4} mx={5}>
+        <Box width="100%" position="relative">
+          <Box
+            position="absolute"
+            bgColor="gray.600"
+            width="100%"
+            height="150px"
+            zIndex={-1}
+          />
+        </Box>
+        <Box mt={2} mx={5}>
           <Box mx="auto">
-            <Box mb={5}>
+            <Box>
               <Heading mb={2} color="white">
                 {headerText}
               </Heading>
               <BreadCrumb breadCrumbs={breadCrumbs}></BreadCrumb>
             </Box>
-            <Box mx="auto">{children}</Box>
+            <Box mx="auto" pt={5} pb={10}>
+              {children}
+            </Box>
           </Box>
         </Box>
       </Box>
