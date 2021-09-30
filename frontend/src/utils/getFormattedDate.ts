4317402f4f9c81f7f7fab0056411a9601e95adc8
Arif Suganda <arifsuganda.su@gmail.com>
2021-09-30 22:32:38 +0800

    fix date using iso string slice

--- frontend/src/utils/getFormattedDate.ts
+++ frontend/src/utils/getFormattedDate.ts
@@ -1,7 +1,3 @@
 export const getFormattedDate = (dateString: string): string => {
-  const date = new Date(dateString);
-  const fullMonth =
-    date.getMonth() < 10 ? `0${date.getMonth()}` : date.getMonth();
-  const fullDate = date.getDate() < 10 ? `0${date.getDate()}` : date.getDate();
-  return date.getFullYear() + "-" + fullMonth + "-" + fullDate;
+  return new Date(dateString).toISOString().slice(0, 10);
 };
