6d0364e5ca260d3c6002dcbaf36250d073f6766c
Arif Suganda <arifsuganda.su@gmail.com>
2021-12-14 22:31:29 +0800

    use authorization to pengguna rutin

--- frontend/src/pages/dashboard/pengguna-rutin/index.tsx
+++ frontend/src/pages/dashboard/pengguna-rutin/index.tsx
@@ -11,7 +11,7 @@ import {
 } from "@chakra-ui/react";
 import NextLink from "next/link";
 import React, { useMemo, useState } from "react";
-import { FaEdit, FaFileDownload, FaTrash } from "react-icons/fa";
+import { FaEdit, FaFileDownload, FaPaperPlane, FaTrash } from "react-icons/fa";
 import { Column } from "react-table";
 import { DashboardLayout } from "../../../components/DashboardLayout";
 import { DeleteDialog } from "../../../components/DeleteDialog";
@@ -22,9 +22,11 @@ import {
   usePenggunaRutinsQuery,
 } from "../../../generated/graphql";
 import { useIsAuth } from "../../../middlewares/useIsAuth";
+import { useRole } from "../../../utils/useRole";
 
 const DashboardPenggunaRutinIndex: React.FC<{}> = ({}) => {
   useIsAuth();
+  const { isOperator } = useRole();
   const breadCrumbs = [
     { text: "Dashboard", link: "/dashboard", isCurrentPage: false },
     { text: "Pengguna Rutin", link: "#", isCurrentPage: true },
@@ -99,6 +101,19 @@ const DashboardPenggunaRutinIndex: React.FC<{}> = ({}) => {
           console.log(cellObj);
           return (
             <HStack spacing={1}>
+              <NextLink
+                href={`/dashboard/pengguna-rutin/detail/${cellObj.row.values.id}`}
+              >
+                <Link>
+                  <IconButton
+                    aria-label="Detail"
+                    size="sm"
+                    bgColor="transparent"
+                    color="green.500"
+                    icon={<FaPaperPlane />}
+                  ></IconButton>
+                </Link>
+              </NextLink>
               <Tooltip label="Download File BAP" aria-label="Download File BAP">
                 <Link href={cellObj.row.original.fileBapUrl} isExternal>
                   <IconButton
@@ -110,30 +125,34 @@ const DashboardPenggunaRutinIndex: React.FC<{}> = ({}) => {
                   ></IconButton>
                 </Link>
               </Tooltip>
-              <NextLink
-                href={`/dashboard/pengguna-rutin/edit/${cellObj.row.values.id}`}
-              >
-                <Link>
-                  <IconButton
-                    aria-label="Ubah"
-                    size="sm"
-                    bgColor="transparent"
-                    color="blue.500"
-                    icon={<FaEdit />}
-                  ></IconButton>
-                </Link>
-              </NextLink>
-              <IconButton
-                size="sm"
-                aria-label="Hapus"
-                bgColor="transparent"
-                color="red.500"
-                icon={<FaTrash />}
-                onClick={() => {
-                  setCurrentRow(cellObj.row.values as PenggunaRutin);
-                  setDeleteDialogOpen(true);
-                }}
-              ></IconButton>
+              {isOperator ? (
+                <NextLink
+                  href={`/dashboard/pengguna-rutin/edit/${cellObj.row.values.id}`}
+                >
+                  <Link>
+                    <IconButton
+                      aria-label="Ubah"
+                      size="sm"
+                      bgColor="transparent"
+                      color="blue.500"
+                      icon={<FaEdit />}
+                    ></IconButton>
+                  </Link>
+                </NextLink>
+              ) : null}
+              {isOperator ? (
+                <IconButton
+                  size="sm"
+                  aria-label="Hapus"
+                  bgColor="transparent"
+                  color="red.500"
+                  icon={<FaTrash />}
+                  onClick={() => {
+                    setCurrentRow(cellObj.row.values as PenggunaRutin);
+                    setDeleteDialogOpen(true);
+                  }}
+                ></IconButton>
+              ) : null}
             </HStack>
           );
         },
@@ -151,13 +170,15 @@ const DashboardPenggunaRutinIndex: React.FC<{}> = ({}) => {
           <Box p={8}>
             <Flex align="center" justifyContent="space-between" mb={8}>
               <Text fontSize="l">Pengguna Rutin</Text>
-              <NextLink href="/dashboard/pengguna-rutin/tambah">
-                <Link>
-                  <Button bg="blue.500" color="white">
-                    Tambah
-                  </Button>
-                </Link>
-              </NextLink>
+              {isOperator ? (
+                <NextLink href="/dashboard/pengguna-rutin/tambah">
+                  <Link>
+                    <Button bg="blue.500" color="white">
+                      Tambah
+                    </Button>
+                  </Link>
+                </NextLink>
+              ) : null}
             </Flex>
             <Box>
               {!loading && data?.penggunaRutins.data ? (
@@ -172,14 +193,16 @@ const DashboardPenggunaRutinIndex: React.FC<{}> = ({}) => {
           </Box>
         </Box>
       </Stack>
-      <DeleteDialog
-        deleteDialogOpen={deleteDialogOpen}
-        deleteDialogCancel={deleteDialogCancel}
-        deleteDialogClose={deleteDialogClose}
-        currentRow={currentRow}
-        dialogKey={dialogKey}
-        deleteConfirm={deleteConfirm}
-      ></DeleteDialog>
+      {isOperator ? (
+        <DeleteDialog
+          deleteDialogOpen={deleteDialogOpen}
+          deleteDialogCancel={deleteDialogCancel}
+          deleteDialogClose={deleteDialogClose}
+          currentRow={currentRow}
+          dialogKey={dialogKey}
+          deleteConfirm={deleteConfirm}
+        ></DeleteDialog>
+      ) : null}
     </DashboardLayout>
   );
 };
